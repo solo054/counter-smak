8a6a0530305ddc5fe6ee29d2db7c6134c3bca1f7
samir-sb <samir.belhadi@univ-batna2.dz>
2020-10-11 22:11:46 +0100

    animate numbers only when they appear on screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,41 @@
-var numbers = document.querySelectorAll('.number');
+const counterSection = document.querySelector('#counter');
+const numbers = document.querySelectorAll('.number');
 const speed = 2500
+options = {
 
-numbers.forEach(number => {
-  const updateCount = () => {
-    const target = +number.getAttribute('data-number');
-    const count = +number.innerText;
+}
 
-    var inc = target / speed * 41.2321;
-    if (count < target) {
-      number.innerText = Math.ceil(count + inc);
-      setTimeout(updateCount, 50)
+const counterObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting) {
+      numbers.forEach(number => {
+        const updateCount = () => {
+          const target = +number.getAttribute('data-number');
+          const count = +number.innerText;
+
+          var inc = target / speed * 41.2321;
+          if (count < target) {
+            number.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 50)
+          } else {
+            number.innerText = target;
+          }
+        }
+
+        updateCount();
+      })
     } else {
-      number.innerText = target;
+      // uncomment this if u want animate the numbers
+      // every times appear on screen.
+      // numbers.forEach(number => {
+      //   number.innerText = 0;
+      // })
     }
-  }
 
-  updateCount();
-})
+  })
+}, options)
+
+
+
+counterObserver.observe(counterSection)
